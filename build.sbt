832873dfe378042b82e9c0abb77cc2d73a44cb4b
Jacek Laskowski <jacek@japila.pl>
2018-06-13 21:42:53 +0200

    scopt

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "scala-workshop-day2"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies += "com.github.scopt" %% "scopt" % "3.7.0"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,50 @@
+
 object Main extends App {
 
-  val path = "src/test/resources/"
-  import java.nio.file.{Files, Paths}
-  val dir = Paths.get(path)
-  import scala.collection.JavaConverters._
-  val files = Files.list(dir).iterator().asScala.toList
+  // How to support two options + --help?
 
-  println(s">>> Files under $path directory:")
+  val parser = new scopt.OptionParser[MyConfig]("runMain Main") {
+    head("Main", "1.x")
+
+    // --path src/test/resources/
+    // -p src/test/resources/
+    opt[String]('p', "path")
+      .action((x, mc) => mc.copy(path = x))
+      .text("path to the files to process")
+
+    opt[String]('m', "no-files-msg")
+      .action((x, mc) => mc.copy(noFilesMsg = x)) // FIXME Explain case class + copy constructor
+      .text("message for 'No files'")
+
+    // --help
+    help("help").text("Prints help")
+  }
+
+  // this is Option type
+  // no value = -1 or null ==> None
+  // some value ==> Some(value)
+  val myConfig = parser.parse(args, MyConfig()).getOrElse { // FIXME Explain Option type + getOrElse
+    // this code is executed only when parsing fails
+    println("Something went wrong. Exiting")
+    sys.exit(-1) // FIXME Explain sys object
+  }
+
+  val _path = myConfig.path
+  val _noFilesMsg = myConfig.noFilesMsg
+
+  import java.nio.file.{Files, Paths}
+  val dir = Paths.get(_path)
+  import scala.collection.JavaConverters._
+  import scala.util.Try
+  // FIXME Explain Try
+  // FIXME Explain Seq.empty without defining the type of elements (type inference)
+  val files = Try(Files.list(dir).iterator().asScala.toList).getOrElse(Seq.empty)
+
+  println(s">>> Files under ${_path} directory:")
   val filesCount = files.length
   if (filesCount == 0) {
-    println("No files")
+    println(_noFilesMsg)
+    sys.exit(0)
   } else {
     files.foreach(println)
   }
@@ -23,3 +58,12 @@ object Main extends App {
   println("\n >>> The app worked fine...you can continue")
 
 }
+
+case class MyConfig(
+  // --path
+  // -p
+  path: String = "src/test/resources/",
+
+  // --no-files-msg
+  // -m
+  noFilesMsg: String = "No files")
